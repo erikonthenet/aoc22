4930d9d5fa83c6a3b49f027492b327354d3aeb2f
erikonthenet <hj@loosman.org>
2022-12-02 09:23:33 +0100

    Update Solution.java

--- day2/Solution.java
+++ day2/Solution.java
@@ -1,4 +1,4 @@
-package day2_2;
+package day2;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,7 +11,11 @@ public class Solution {
         "A", Shape.ROCK,
         "B", Shape.PAPERS,
         "C", Shape.SCISSORS);
-    static Map<String, Outcome> XYZ_MAP = Map.of(
+    static Map<String, Shape> XYZ_MAP_1 = Map.of(
+        "X", Shape.ROCK,
+        "Y", Shape.PAPERS,
+        "Z", Shape.SCISSORS);
+    static Map<String, Outcome> XYZ_MAP_2 = Map.of(
         "X", Outcome.LOST,
         "Y", Outcome.DRAW,
         "Z", Outcome.WON);
@@ -20,23 +24,62 @@ public class Solution {
         var lines = Files.readAllLines(Path.of("day2/input.txt"));
         var shapeScore = new AtomicInteger(0);
         var outcomeScore = new AtomicInteger(0);
+
+        // part 1
         lines.forEach(line -> {
             var pair = line.split(" ");
-            var round = new Round(
+            var round = new Round1(
                 ABC_MAP.get(pair[0]),
-                XYZ_MAP.get(pair[1])
+                XYZ_MAP_1.get(pair[1])
+            );
+
+            shapeScore.addAndGet(round.myChoice.value);
+            outcomeScore.addAndGet(calculateOutcome(round));
+        });
+
+        System.out.println("=== Part 1 ===");
+        System.out.println("Shape score = " + shapeScore.get());
+        System.out.println("Outcome score = " + outcomeScore.get());
+        System.out.println("Total score = " + (shapeScore.get() + outcomeScore.get()));
+
+        // part 2
+        shapeScore.set(0);
+        outcomeScore.set(0);
+
+        lines.forEach(line -> {
+            var pair = line.split(" ");
+            var round = new Round2(
+                ABC_MAP.get(pair[0]),
+                XYZ_MAP_2.get(pair[1])
             );
 
             shapeScore.addAndGet(calculateMyChoice(round));
             outcomeScore.addAndGet(round.outcome.value);
         });
 
+        System.out.println("=== Part 2 ===");
         System.out.println("Shape score = " + shapeScore.get());
         System.out.println("Outcome score = " + outcomeScore.get());
         System.out.println("Total score = " + (shapeScore.get() + outcomeScore.get()));
     }
 
-    private static int calculateMyChoice(Round round) {
+    private static int calculateOutcome(Round1 round) {
+        var oppChoice = round.oppChoice;
+        var myChoice = round.myChoice;
+
+        if (oppChoice == myChoice) {
+            return Outcome.DRAW.value;
+        }
+        if (  (oppChoice == Shape.ROCK && myChoice == Shape.PAPERS)
+           || (oppChoice == Shape.PAPERS && myChoice == Shape.SCISSORS)
+           || (oppChoice == Shape.SCISSORS && myChoice == Shape.ROCK)) {
+            return Outcome.WON.value;
+        }
+
+        return Outcome.LOST.value;
+    }
+
+    private static int calculateMyChoice(Round2 round) {
         var oppChoice = round.oppChoice;
         var outcome = round.outcome;
 
@@ -56,7 +99,9 @@ public class Solution {
         return Shape.ROCK.value;
     }
 
-    static record Round(Shape oppChoice, Outcome outcome) {};
+    static record Round1(Shape oppChoice, Shape myChoice) {};
+    static record Round2(Shape oppChoice, Outcome outcome) {};
+
 
     static enum Shape {
         ROCK(1),
