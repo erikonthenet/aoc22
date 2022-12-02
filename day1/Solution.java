a9b733eaa13809825fc653b5e1162dc2bab1de6e
erikonthenet <hj@loosman.org>
2022-12-02 14:33:35 +0100

    Update Solution.java

--- day1/Solution.java
+++ day1/Solution.java
@@ -13,11 +13,9 @@ class Solution {
         List<Integer> elfTotalCalories = new ArrayList<>();
         
         int currentElfTotalCalories = 0;
-        int aantalLegeRegels = 0;
 
         for(int i = 0; i < lines.size(); i++) {
             if (lines.get(i).equals("")) {
-                aantalLegeRegels++;
                 elfTotalCalories.add(currentElfTotalCalories);
                 currentElfTotalCalories = 0;
             } else {
@@ -26,8 +24,6 @@ class Solution {
         }
         elfTotalCalories.add(currentElfTotalCalories);
 
-        System.out.println("Aantal regels: " + lines.size());
-        System.out.println("Aantal lege regels: " + aantalLegeRegels);
         System.out.println("Aantal elven: " + elfTotalCalories.size());
         System.out.println(elfTotalCalories.stream().max(Comparator.naturalOrder()));
         System.out.println(elfTotalCalories.stream().min(Comparator.naturalOrder()));
