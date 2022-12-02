bce170ee4ac121830701e9064f8e3571e77f2563
erikonthenet <hj@loosman.org>
2022-12-02 12:50:48 +0100

    Day 1 in Kotlin

---
+++ day1/Kotsolution.kt
@@ -0,0 +1,23 @@
+import java.io.File
+
+fun main() {
+    var elfTotalCalories = mutableListOf<Int>()
+    var currentElfCalories = 0;
+
+    File("input.txt").forEachLine {
+        if (it == "") {
+            elfTotalCalories.add(currentElfCalories)
+            currentElfCalories = 0
+        } else {
+            currentElfCalories += it.toInt()
+        }
+    }
+    elfTotalCalories.add(currentElfTotalCalories)
+    elfTotalCalories.sortDescending()
+
+    println("Aantal elven: ${elfTotalCalories.size}")
+    println("Max: " + elfTotalCalories.first())
+    println("Min: " + elfTotalCalories.last())
+
+    println("Top 3 heeft ${elfTotalCalories.take(3).sum()}")
+}
